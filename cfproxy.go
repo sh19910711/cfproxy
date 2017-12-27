9af92b1cac7f05ef11876ada29552bb4a532081b
Hiroyuki Sano <sh19910711@gmail.com>
2017-12-27 09:29:51 +0000

    replace with template code

--- cfproxy.go
+++ cfproxy.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/elazarl/goproxy"
 	"net/http"
+	"os"
+	"regexp"
 )
 
 func main() {
@@ -12,6 +14,15 @@ func main() {
 	proxy.OnResponse(goproxy.DstHostIs("codeforces.com")).DoFunc(
 		func(res *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 			res.Body = Replace(res)
+			return res
+		})
+
+	proxy.OnResponse(goproxy.UrlMatches(regexp.MustCompile("/submit$"))).DoFunc(
+		func(res *http.Response, ctxt *goproxy.ProxyCtx) *http.Response {
+			template := "./template"
+			if _, err := os.Stat(template); err == nil {
+				res.Body = ReplaceSourceCode(res, template)
+			}
 			return res
 		})
 
--- replace.go
+++ replace.go
@@ -16,3 +16,13 @@ func Replace(res *http.Response) io.ReadCloser {
 	r, _ := doc.Html()
 	return ioutil.NopCloser(bytes.NewBufferString(r))
 }
+
+func ReplaceSourceCode(res *http.Response, path string) io.ReadCloser {
+	doc, _ := goquery.NewDocumentFromResponse(res)
+
+	code, _ := ioutil.ReadFile(path)
+	doc.Find("#sourceCodeTextarea").SetText(string(code))
+
+	r, _ := doc.Html()
+	return ioutil.NopCloser(bytes.NewBufferString(r))
+}
